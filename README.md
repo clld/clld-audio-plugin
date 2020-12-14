1f48bc6ced14a43c67371965ff6c86a48cee05ec
xrotwang <xrotwang@googlemail.com>
2020-12-14 14:48:10 +0100

    tests and scaffolding

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # clld-audio-plugin
+
+[![Build Status](https://github.com/clld/clld-audio-plugin/workflows/tests/badge.svg)](https://github.com/clld/clld-audio-plugin/actions?query=workflow%3Atests)
+[![codecov](https://codecov.io/gh/clld/clld-audio-plugin/branch/master/graph/badge.svg)](https://codecov.io/gh/clld/clld-audio-plugin)
+[![PyPI](https://img.shields.io/pypi/v/clld-audio-plugin.svg)](https://pypi.org/project/clld-audio-plugin)
+
--- tests/test_misc.py
+++ tests/test_misc.py
@@ -2,15 +2,25 @@ from clld_audio_plugin import includeme
 from clld_audio_plugin.datatables import *
 from clld_audio_plugin.maps import ParamMap
 from clld_audio_plugin.util import *
+from clld_audio_plugin import models
 
 
 def test_includeme(mocker):
+    assert models
     includeme(mocker.MagicMock())
 
 
-def test_AudioCol():
-    pass
+def test_AudioCol(mocker):
+    assert AudioCol(mocker.MagicMock(), '').format(mocker.Mock(audio='x'))
+    assert not AudioCol(mocker.MagicMock(), '').format(mocker.Mock(audio=None))
 
 
-def test_form2audio():
-    pass
+def test_form2audio(mocker):
+    res =form2audio(mocker.Mock(
+        iter_rows=lambda *args: [dict(mimetype='audio/mpeg', formReference='x')],
+        get_row_url=lambda *args: 'http://example.org'))
+    assert res['x'] == 'http://example.org'
+
+
+def test_ParamMap(mocker):
+    assert ParamMap(mocker.Mock(), mocker.Mock()).options['with_audioplayer']
